9a5fba6dce7f9a30d51696a04616bcfb85e867f5
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-01-10 22:38:34 -0300

    configuration express, view engine and database

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const connection = require('./database/database');
+
 const app = express();
 const port = 3000;
 
+// view engine
+app.set('view engine', 'ejs');
+
+// static
+app.use(express.static('public'))
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// database
+connection.authenticate()
+    .then(() => {
+        console.log("Aplicação conectada com banco de dados");
+    })
+    .catch(error => {
+        console.error("Erro ao conectar com banco de dados:", error);
+    })
+
 app.get("/", (req, res) => {
-    res.send("Bem vindo")
+    res.render("index")
 })
 
 app.listen(port, () => {
