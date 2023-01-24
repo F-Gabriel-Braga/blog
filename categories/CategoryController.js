31f21c8d630d815824f518b96c5a09c66590b6be
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-01-23 21:51:28 -0300

    page new category

--- categories/CategoryController.js
+++ categories/CategoryController.js
@@ -6,4 +6,8 @@ router.get("/categories", (req, res) => {
     res.send("Categorias");
 });
 
+router.get("/admin/categories/new", (req, res) => {
+    res.render('admin/categories/new')
+});
+
 module.exports = router;
